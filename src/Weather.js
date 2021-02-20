f056021956c56633a3dae1504382c0d37ed710f9
Elina Osmala <n0osel00@students.oamk.fi>
2021-02-20 17:05:15 +0200

    apikey poisto

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 const ICON_URL = 'http://openweathermap.org/img/wn/';
-const API_KEY = '1fc62f60496b4d84281e1159772f2507';
+const API_KEY = '';
 
 
 export default function Weather({lat, lng}) {
